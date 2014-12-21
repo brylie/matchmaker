f2f7e299b0f44bb66e868988a8f372e9aa37c1f5
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-12-21 19:32:50 +0200

    Add Query form.

--- collections/queries.js
+++ collections/queries.js
@@ -1,1 +1,7 @@
 Queries = new Meteor.Collection('queries');
+Queries.attachSchema(new SimpleSchema({
+    query: {
+        type: String,
+        label: "Query"
+    }
+}));
--- collections/responses.js
+++ collections/responses.js
@@ -1,1 +1,11 @@
 Responses = new Meteor.Collection('responses');
+Responses.attachSchema(new SimpleSchema({
+    response: {
+        type: String,
+        label: "Response"
+    },
+    author: {
+        type: String,
+        label: "Author"
+    }
+}));
