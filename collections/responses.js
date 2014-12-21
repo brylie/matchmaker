ed34f68410eb0c9fcaafdc23209fd4b4a1a4bb8d
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-12-21 21:28:58 +0200

    Made queries link to query pages.

--- collections/responses.js
+++ collections/responses.js
@@ -2,6 +2,7 @@ Responses = new Meteor.Collection('responses');
 Responses.attachSchema(new SimpleSchema({
     response: {
         type: String,
+        max: 1000,
         label: "Response"
     },
     author: {
@@ -9,6 +10,13 @@ Responses.attachSchema(new SimpleSchema({
         label: "Author",
         autoValue: function () {
             return Meteor.userId();
+        }
+    },
+    toQuery: {
+        type: String,
+        label: "Query ID",
+        autoValue: function () {
+            return this._id;
         }
     }
 }));
