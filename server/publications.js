dbf44c96c76934aa83177057678ce88f2576794a
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-12-21 20:05:41 +0200

    Switched to Bootstrap 3. Autoform modals, navbar.

---
+++ server/publications.js
@@ -0,0 +1,10 @@
+// Get responses for a given Query
+Meteor.publish('queryResponses', function(queryId) {
+    return Responses.find({queryId: queryId});
+});
+
+//Try to get the Query Responses by URL slug instead of DB Id?
+//Meteor.publish('queryResponses', function(slug) {
+//    query = Query.findOne({slug: slug});
+//    return Responses.find({queryId: query._id});
+//});
--- server/server.js
+++ server/server.js
@@ -1,7 +1,3 @@
-Meteor.publish('queryResponses', function(queryId) {
-    return Responses.find({queryId: queryId});
-});
-
 Meteor.startup(function () {
     // code to run on server at startup
 });
