9ce69ba8aa08c8a157374f568b5c96e8b5685eb3
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-12-22 18:00:59 +0200

    Used regular form instead of autoform.

--- client/templates/queries.js
+++ client/templates/queries.js
@@ -3,3 +3,22 @@ Template.queries.helpers({
         return Queries.find().fetch();
     }
 });
+
+Template.submitQuery.events({
+    'click #submitQueryButton' : function (event) {
+        event.preventDefault();
+
+        // Get the query values
+        var query = $('#yourQuery').val();
+        var authorId = Meteor.userId();
+
+        // Insert the response into the database
+        Queries.insert({
+            query: query,
+            authorId: authorId
+        });
+
+        // Reset the form field value to blank
+        $('#yourQuery').val('');
+    }
+});
--- client/templates/query.js
+++ client/templates/query.js
@@ -1,6 +1,9 @@
 Template.showResponses.helpers({
-    'responses' : function () {
+    responses : function () {
+        // Set a session variable to be used in the addResponse template
         Session.set('queryId', this._id);
+
+        // Get all responses related to the current query
         return Responses.find({queryId: this._id});
     }
 });
