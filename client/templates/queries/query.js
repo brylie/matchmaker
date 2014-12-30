c501ebba67d1497056dec1a3c4f174bf67344fcb
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-12-31 01:30:33 +0200

    Refactoring show responses template helpers.

--- client/templates/queries/query.js
+++ client/templates/queries/query.js
@@ -1,13 +1,3 @@
-Template.showResponses.helpers({
-    responses : function () {
-        // Set a session variable to be used in the addResponse template
-        Session.set('queryId', this._id);
-
-        // Get all responses related to the current query
-        return Responses.find({queryId: this._id});
-    }
-});
-
 Template.addResponse.events({
     'click #submitResponse' : function (event) {
         event.preventDefault();
